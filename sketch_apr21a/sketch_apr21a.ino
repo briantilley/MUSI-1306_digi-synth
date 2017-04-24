e089ac2fc2d51dd0cfb7ca0c73eb1a85a77a3ed5
Brian Tilley <briantilley97@gmail.com>
2017-04-23 22:38:40 -0500

    increments through base-level fundamental tones

--- sketch_apr21a/sketch_apr21a.ino
+++ sketch_apr21a/sketch_apr21a.ino
@@ -9,7 +9,11 @@ uint16_t fundamentals[] = { NOTE_F2, NOTE_FS2, NOTE_G2, NOTE_GS2, NOTE_A2,
 	NOTE_AS2, NOTE_B2, NOTE_C3, NOTE_CS3, NOTE_D3, NOTE_DS3, NOTE_E3,
 	NOTE_F3, NOTE_FS3, NOTE_G3, NOTE_GS3, NOTE_A3, NOTE_AS3, NOTE_B3,
 	NOTE_C4, NOTE_CS4, NOTE_D4 };
+#define SIZE_FUNDAMENTALS 22
 uint8_t baseFundamentalIdx = 0;
+
+// for fun
+long f_ts;
 
 void setup() {
 	// harmonic selection
@@ -21,6 +25,8 @@ void setup() {
 	P2DIR = 0x00;
 	P2REN = 0x07;
 	P2OUT = 0x07;
+
+	f_ts = millis();
 }
 
 void loop() {
@@ -32,39 +38,19 @@ void loop() {
 
 	// artificial valving - augment by a certain number of half steps
 	prev_fundamentalTone = fundamentalTone;
-	switch(~P2IN & 0x07) {
-		case 0x00:
-			fundamentalTone = NOTE_C3;
-			break;
-		case 0x01:
-			fundamentalTone = NOTE_CS3;
-			break;
-		case 0x02:
-			fundamentalTone = NOTE_D3;
-			break;
-		case 0x03:
-			fundamentalTone = NOTE_DS3;
-			break;
-		case 0x04:
-			fundamentalTone = NOTE_E3;
-			break;
-		case 0x05:
-			fundamentalTone = NOTE_F3;
-			break;
-		case 0x06:
-			fundamentalTone = NOTE_FS3;
-			break;
-		case 0x07:
-			fundamentalTone = NOTE_G3;
-			break;
-		default:
-			fundamentalTone = NOTE_C3;
-			break;
-	}
+	fundamentalTone = fundamentals[baseFundamentalIdx + (~P2IN & 0x07)];
 
 	// play the valve-augmented root of the harmonic series
 	if(harmonic != prev_harmonic || fundamentalTone != prev_fundamentalTone) {
 		analogFrequency(fundamentalTone * harmonic);
 		analogWrite(AUDIO_PIN, (harmonic & 0x02) ? 127 : 0); // "0" harmonic is silent (no buttons pressed)
+	}
+
+	if(millis() - 250 > f_ts) {
+		f_ts = millis();
+		baseFundamentalIdx++;
+		if(baseFundamentalIdx > SIZE_FUNDAMENTALS - 8) {
+			baseFundamentalIdx = 0;
+		}
 	}
 }
