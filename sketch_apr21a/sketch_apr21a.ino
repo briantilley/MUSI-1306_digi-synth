42b1e657bbfc3508ec777a775ff1c20c8ddd88bb
Brian Tilley <briantilley97@gmail.com>
2017-04-23 22:31:37 -0500

    working version (no frills, just controls)

--- sketch_apr21a/sketch_apr21a.ino
+++ sketch_apr21a/sketch_apr21a.ino
@@ -1,54 +1,70 @@
 #include "pitches.h"
 
-#define AUDIO_PIN P1_2
+#define AUDIO_PIN P1_6
 
-uint16_t fundamentalTone = NOTE_F4;
+uint16_t fundamentalTone, prev_fundamentalTone;
+uint8_t harmonic, prev_harmonic;
+
+uint16_t fundamentals[] = { NOTE_F2, NOTE_FS2, NOTE_G2, NOTE_GS2, NOTE_A2,
+	NOTE_AS2, NOTE_B2, NOTE_C3, NOTE_CS3, NOTE_D3, NOTE_DS3, NOTE_E3,
+	NOTE_F3, NOTE_FS3, NOTE_G3, NOTE_GS3, NOTE_A3, NOTE_AS3, NOTE_B3,
+	NOTE_C4, NOTE_CS4, NOTE_D4 };
+uint8_t baseFundamentalIdx = 0;
 
 void setup() {
+	// harmonic selection
+	P1DIR = 0x00;
+	P1REN = 0x03;
+	P1OUT = 0x03;
+
+	// fundamental selection
 	P2DIR = 0x00;
 	P2REN = 0x07;
 	P2OUT = 0x07;
-
-	pinMode(PUSH2, INPUT_PULLUP);
 }
 
-int i = 1;
 void loop() {
-	// switch(P2IN & 0x07) {
-	// 	case 0x07:
-	// 		fundamentalTone = NOTE_F3;
-	// 		break;
-	// 	case 0x06:
-	// 		fundamentalTone = NOTE_FS3;
-	// 		break;
-	// 	case 0x05:
-	// 		fundamentalTone = NOTE_G3;
-	// 		break;
-	// 	case 0x04:
-	// 		fundamentalTone = NOTE_GS3;
-	// 		break;
-	// 	case 0x03:
-	// 		fundamentalTone = NOTE_A3;
-	// 		break;
-	// 	case 0x02:
-	// 		fundamentalTone = NOTE_AS3;
-	// 		break;
-	// 	case 0x01:
-	// 		fundamentalTone = NOTE_B3;
-	// 		break;
-	// 	case 0x00:
-	// 		fundamentalTone = NOTE_C4;
-	// 		break;
-	// 	default:
-	// 		fundamentalTone = NOTE_F3;
-	// 		break;
-	// }
 
-	// analogFrequency(fundamentalTone);
-	analogFrequency(fundamentalTone * i++);
-	if(i > 12) i = 1;
-	analogWrite(AUDIO_PIN, 127);
-	// analogWrite(AUDIO_PIN, digitalRead(PUSH2) ? 0 : 127);
-	delay(500);
+	// read port 1 for the harmonic (0x01 == first harmonic, 0x02 == second ...)
+	// must invert input logic to account for pull-up resistor
+	prev_harmonic = harmonic;
+	harmonic = ~P1IN & 0x07;
+
+	// artificial valving - augment by a certain number of half steps
+	prev_fundamentalTone = fundamentalTone;
+	switch(~P2IN & 0x07) {
+		case 0x00:
+			fundamentalTone = NOTE_C3;
+			break;
+		case 0x01:
+			fundamentalTone = NOTE_CS3;
+			break;
+		case 0x02:
+			fundamentalTone = NOTE_D3;
+			break;
+		case 0x03:
+			fundamentalTone = NOTE_DS3;
+			break;
+		case 0x04:
+			fundamentalTone = NOTE_E3;
+			break;
+		case 0x05:
+			fundamentalTone = NOTE_F3;
+			break;
+		case 0x06:
+			fundamentalTone = NOTE_FS3;
+			break;
+		case 0x07:
+			fundamentalTone = NOTE_G3;
+			break;
+		default:
+			fundamentalTone = NOTE_C3;
+			break;
+	}
+
+	// play the valve-augmented root of the harmonic series
+	if(harmonic != prev_harmonic || fundamentalTone != prev_fundamentalTone) {
+		analogFrequency(fundamentalTone * harmonic);
+		analogWrite(AUDIO_PIN, (harmonic & 0x02) ? 127 : 0); // "0" harmonic is silent (no buttons pressed)
+	}
 }
-
